174301059257dfc5088dc5d697f7a61c63929375
Juv1e <Juv1e@users.noreply.github.com>
2018-07-21 19:42:12 +0300

    релиз моего говна
    убейте меня пожалуйста

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(33, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(261, 273);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(102, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(141, 215);
+            this.progressBar1.Maximum = 3000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(114, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "RETROWAVE Radio is NOT playing!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Download";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(261, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Pause";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 305);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "RETROWAVE Radio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Threading;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        static string urltrack = "";
+        static string trackname = "";
+        public static readonly string DIR_BASE = AppDomain.CurrentDomain.BaseDirectory;
+        public static readonly string DIR_DL = DIR_BASE + "Downloads";
+        bool IsFirstClick = true;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Start();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Start()
+        {
+            string url = "http://retrowave.ru";
+            var parse = APIParse();
+            string name = parse["body"]["tracks"][0]["title"].ToString();
+            string fixedname = name.Replace("вЂ“", "–");
+            string track = parse["body"]["tracks"][0]["streamUrl"].ToString();
+            string art = parse["body"]["tracks"][0]["artworkUrl"].ToString();
+            wplayer.URL = url + track;
+            urltrack = wplayer.URL;
+            trackname = fixedname + ".mp3";
+            label1.Text = "Now playing: " + fixedname;
+            pictureBox1.Load(url + art);
+            wplayer.controls.play();
+            button1.Text = "Next";
+            wplayer.settings.volume = trackBar1.Value;
+            timer1.Start();
+        }
+        public void Stop()
+        {
+            pictureBox1.Load("http://retrowave.ru/img/share.png");
+            button1.Text = "Play";
+            label1.Text = "Title";
+            wplayer.controls.stop();
+        }
+        static JObject APIParse()
+        {
+            Random rnd = new Random();
+            string cursor = rnd.Next(0, 399).ToString();
+            var s = cursor;
+            WebClient wc = new WebClient();
+            wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
+            string jsonResponse = wc.DownloadString("http://retrowave.ru/api/v1/tracks?cursor=" + s + "&limit=1");
+            JObject parse = JObject.Parse(jsonResponse);
+            return parse;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.pause();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wplayer.settings.volume = trackBar1.Value;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double t = Math.Floor(wplayer.controls.currentPosition);
+            double d = Math.Floor(wplayer.currentMedia.duration);
+            int time = Convert.ToInt32(d);
+            int timer = Convert.ToInt32(t);
+            progressBar1.Maximum = time;
+            progressBar1.Value = timer;
+            int sec = timer;
+            int minutes = sec / 60;
+            int newSec = sec - minutes * 60;
+            int hour = minutes / 60;
+            int newMinnutes = minutes - hour * 60;
+            TimeSpan times = new TimeSpan(hour, newMinnutes, newSec);
+
+            int secs = time;
+            int minutess = secs / 60;
+            int newSecs = secs - minutess * 60;
+            int hours = minutess / 60;
+            int newMinnutess = minutess - hours * 60;
+            TimeSpan timess = new TimeSpan(hours, newMinnutess, newSecs);
+            label2.Text = times.ToString() + "/" + timess;
+            string login = label2.Text.Split('/')[0].Trim();
+            string password = label2.Text.Split('/')[1].Trim();
+            if (password != "00:00:00")
+            {
+                if (login == password)
+                {
+                    string url = "http://retrowave.ru";
+                    var parse = APIParse();
+                    string name = parse["body"]["tracks"][0]["title"].ToString();
+                    string fixedname = name.Replace("вЂ“", "–");
+                    string track = parse["body"]["tracks"][0]["streamUrl"].ToString();
+                    string art = parse["body"]["tracks"][0]["artworkUrl"].ToString();
+                    wplayer.URL = url + track;
+                    label1.Text = "Now playing: " + fixedname;
+                    pictureBox1.Load(url + art);
+                    wplayer.controls.play();
+                }
+                else { }
+            }
+                if (wplayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                {
+                    label3.Text = "RETROWAVE Radio is playing!";
+                }
+                else
+                {
+                    label3.Text = "RETROWAVE Radio is NOT playing!";
+                }
+        }
+
+        private void progressBar1_Click_1(object sender, EventArgs e)
+        {
+            double t = Math.Floor(wplayer.controls.currentPosition);
+            int timer = Convert.ToInt32(t);
+            progressBar1.Value = timer;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button3_Click_1(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                {
+                    try
+                    {
+                        if (!Directory.Exists(DIR_DL))
+                        {
+                            Directory.CreateDirectory(DIR_DL);
+                        }
+                        WebClient wc = new WebClient();
+                        wc.DownloadFile(urltrack, string.Format(DIR_DL + "\\" + trackname, Guid.NewGuid().ToString()));
+                        MessageBox.Show("Трек успешно сохранен в " + DIR_DL + "\\" + trackname);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Что-то пошло не так.\nТекст ошибки: " + ex.Message);
+                    }
+                }
+            });
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (IsFirstClick) //IsFirstClick = true
+            {
+                wplayer.controls.pause();
+                button4.Text = "Play";
+                IsFirstClick = false;
+            }
+            else // IsFirstClick = false
+            {
+                wplayer.controls.play();
+                button4.Text = "Pause";
+                IsFirstClick = true;
+            }
+        }
+    }
+    }
